9720be288f551ac30521b86e3e1cb09479f0b0d6
willyi67 <hunterestain8@gmail.com>
2026-09-18 12:49:28 +0000

    completed bouncing box project

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,7 +18,13 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(118, 0, 233)"); // bottom wall
+    createPlatform(
+      -50,
+      canvas.height - 10,
+      canvas.width + 100,
+      200,
+      "rgb(118, 0, 233)",
+    ); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
@@ -27,24 +33,23 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
-
+    //toggleGrid();
 
     // TODO 2 - Create Platforms
-
-
-
+    createPlatform(1000, 660, 100, 20, "orange");
+    createPlatform(200, 660, 100, 20, "red");
+    createPlatform(400, 540, 100, 20, "yellow");
+    createPlatform(600, 470, 100, 20, "green");
+    createPlatform(800, 540, 100, 20, "blue");
 
     // TODO 3 - Create Collectables
-
-
-
-    
+    createCollectable("steve", 850, 170, 0.5, 0.7);
+    createCollectable("diamond", 400, 170, 0.5, 0.7);
+    createCollectable("max", 600, 170, 0.5, 0.7);
     // TODO 4 - Create Cannons
-
-
-    
-    
+createCannon("top", 300, 2000);
+createCannon("right", 500, 1000);
+createCannon("left", 203, 2000);
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
